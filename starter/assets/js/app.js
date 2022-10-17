cb4f243d5b7a1edff13997e1b4e59edfa796e5ed
hicham <hicham.elarfaouy.youcode@gmail.com>
2022-10-17 20:19:54 +0100

    complete read function

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -2,7 +2,74 @@
  * In this file app.js you will find all CRUD functions name.
  * 
  */
-createTask();
+let todoTasks = document.getElementById("to-do-tasks");
+let inProgressTasks = document.getElementById("in-progress-tasks");
+let doneTasks = document.getElementById("done-tasks");
+
+readTasks();
+saveTask();
+
+function readTasks(){
+    let count = 0;
+    tasks.forEach(element => {
+        count++;
+        if(element['status'] == 'To Do'){
+            todoTasks.innerHTML += `
+                <button class="border-0 bg-white py-3 d-flex text-start mb-1px rounded-bottom">
+                    <div class="px-3 py-1">
+                        <i class="fa-regular fa-circle-question text-success fa-lg"></i>
+                    </div>
+                    <div class="">
+                        <div class="fw-bold">${element['title']}</div>
+                        <div class="">
+                            <div class="text-black-50">#${count} created in ${element['date']}</div>
+                            <div class="" title="">${element['description']}</div>
+                        </div>
+                        <div class="">
+                            <span class="bg-primary rounded text-white">${element['priority']}</span>
+                            <span class="bg-light rounded">${element['type']}</span>
+                        </div>
+                    </div>
+                </button>`;
+        }else if(element['status'] == 'In Progress'){
+            inProgressTasks.innerHTML += `
+                <button class="border-0 bg-white py-3 d-flex text-start mb-1px rounded-bottom">
+                    <div class="px-3 py-1">
+                        <i class="fa-regular fa-circle-question text-success fa-lg"></i>
+                    </div>
+                    <div class="">
+                        <div class="fw-bold">${element['title']}</div>
+                        <div class="">
+                            <div class="text-black-50">#${count} created in ${element['date']}</div>
+                            <div class="" title="">${element['description']}</div>
+                        </div>
+                        <div class="">
+                            <span class="bg-primary rounded text-white">${element['priority']}</span>
+                            <span class="bg-light rounded">${element['type']}</span>
+                        </div>
+                    </div>
+                </button>`;
+        }else if(element['status'] == 'Done'){
+            doneTasks.innerHTML += `
+                <button class="border-0 bg-white py-3 d-flex text-start mb-1px rounded-bottom">
+                    <div class="px-3 py-1">
+                        <i class="fa-regular fa-circle-question text-success fa-lg"></i>
+                    </div>
+                    <div class="">
+                        <div class="fw-bold">${element['title']}</div>
+                        <div class="">
+                            <div class="text-black-50">#${count} created in ${element['date']}</div>
+                            <div class="" title="">${element['description']}</div>
+                        </div>
+                        <div class="">
+                            <span class="bg-primary rounded text-white">${element['priority']}</span>
+                            <span class="bg-light rounded">${element['type']}</span>
+                        </div>
+                    </div>
+                </button>`;
+        }
+    });
+}
 
 function createTask() {
     // initialiser task form
@@ -10,17 +77,64 @@ function createTask() {
     // Afficher le boutton save
 
     // Ouvrir modal form
-    
+
 }
 
 function saveTask() {
+    let buttonSave = document.getElementById("buttonSave");
+
     // Recuperer task attributes a partir les champs input
+    let taskTitle = document.querySelector("#taskTitle");
+    let taskType = document.getElementById("feature");
+    let taskPriority = document.getElementById("taskPriority");
+    let taskStatus = document.getElementById("taskStatus");
+    let taskDate = document.getElementById("taskDate");
+    let taskDescription = document.getElementById("taskDescription");
+
+    buttonSave.addEventListener("click", (e)=>{
+        e.preventDefault();
+        console.log(taskTitle.childNodes);
+        console.log(taskType.childNodes);
+        console.log(taskPriority);
+        console.log(taskStatus);
+        console.log(taskDate);
+        console.log(taskDescription);
+        if(taskTitle.value !== "" && taskDate.value !== ""){
+            todoTasks.innerHTML += `
+            <button class="border-0 bg-white py-3 d-flex text-start mb-1px rounded-bottom">
+                <div class="px-3 py-1">
+                    <i class="fa-regular fa-circle-question text-success fa-lg"></i>
+                </div>
+                <div class="">
+                    <div class="fw-bold">Use charts and diagrams</div>
+                    <div class="">
+                        <div class="text-black-50">#5 created in 2022-10-08</div>
+                        <div class="" title="While it is not always necessary, sometimes it might be beneficial to prepare a flowchart, a block diagram or some other kind of concept visualization that will render it easy for the developer to comprehend the task and its scope.">While it is not always necessary, sometimes it might be...</div>
+                    </div>
+                    <div class="">
+                        <span class="bg-primary rounded text-white">High</span>
+                        <span class="bg-light rounded">Feature</span>
+                    </div>
+                </div>
+            </button>`;
+        }
+    });
 
     // Créez task object
+    // let task = {
+    //     'title'         :   taskTitle,
+    //     'type'          :   taskType,
+    //     'priority'      :   taskPriority,
+    //     'status'        :   taskStatus,
+    //     'date'          :   taskDate,
+    //     'description'   :   taskDescription,
+    // };
 
     // Ajoutez object au Array
+    // tasks.push(task);
 
     // refresh tasks
+    // readTasks();
     
 }
 
